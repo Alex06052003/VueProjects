ae5c8ea1b39fc2862886455b81e52b11b218bedf
AR1D <84451649+Alex06052003@users.noreply.github.com>
2024-06-25 14:30:43 +0800

    Add files via upload

---
+++ Octogon5Lvl/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import TheNavigation from "./components/TheNavigation.vue";
+export default {
+    data() {
+        return {};
+    },
+    components: {
+        TheNavigation,
+    },
+};
+</script>
+
+<template>
+    <the-navigation />
+</template>
+
+<style scoped></style>
---
+++ Octogon5Lvl/src/components/TheNavigation.vue
@@ -0,0 +1,37 @@
+<script></script>
+
+<template>
+    <header class="header">
+        <a href="/" class="header-logo">
+            <img
+                src="../assets/logo.png"
+                alt="Logoipsum logo"
+                width="199"
+                height="39"
+                class="header-logo-image"
+                loading="lazy"
+            />
+        </a>
+        <nav class="header-menu">
+            <ul class="header-menu-list">
+                <li class="header-menu-item">
+                    <a href="/" class="header-menu-link">Home</a>
+                </li>
+                <li class="header-menu-item">
+                    <a href="/" class="header-menu-link">Features</a>
+                </li>
+                <li class="header-menu-item">
+                    <a href="/" class="header-menu-link">Pricing</a>
+                </li>
+                <li class="header-menu-item">
+                    <a href="/" class="header-menu-link">Blog</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="header-action">
+            <button class="header-book-button button"></button>
+        </div>
+    </header>
+</template>
+
+<style scoped></style>
